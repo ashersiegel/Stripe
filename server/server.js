0df239d485725e4eabb83442c38cd38ee445c900
Asher Siegel <ashersiegel@icloud.com>
2023-08-07 17:36:49 -0500

    new

--- server/server.js
+++ server/server.js
@@ -4,7 +4,13 @@ const express = require("express")
 const app = express()
 
 app.use(express.json())
+app.use(express.static(public))
 
 const stripe = require("stripe")(process.env.STRIPE_PRIVATE_KEY)
+
+const storeItens = new Map([
+    [1, {priceInCents: 1000, name: "Basic Subscription"}],
+    [2, {priceInCents: 1500, name: "Premimum Subscription"}]
+])
 
 app.listen(3000)
